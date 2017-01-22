c72f703cffb8d29cd49aed4c01433ae36b0bd10b
Peter Harrison <blackriver@simiya.com>
2017-01-21 17:13:49 -0800

    Fixed 'mdl --get' option on the test_installation.py script

--- mdl/api/get/coordinates.py
+++ mdl/api/get/coordinates.py
@@ -1,6 +1,9 @@
 """mdl-ng database API. Datapoint table."""
 
 # Standard imports
+from collections import defaultdict
+
+# PIP3 imports
 import requests
 
 # Flask imports
@@ -26,36 +29,7 @@ def get_lastgps_drivers():
 
     """
     # Initialize key variables
-    data = []
-
-    # Get data from cache
-    key = ('mdl.api:get/coordinates/lastcontactsdrivers')
-    cache_value = CACHE.get(key)
-
-    # Get infoset URI to process
-    uri = 'db/multitable/datapointsummary'
-    url = '{}/{}'.format(reference.infoset_api_url_prefix(CONFIG), uri)
-
-    # Process cache miss
-    if cache_value is None:
-        # Get data from infoset
-        try:
-            result = requests.get(url)
-            received_data = result.json()
-        except:
-            received_data = None
-
-        # Filter data
-        if received_data is not None:
-            for item in received_data:
-                if item['name'] != 'DoRoad':
-                    continue
-                data.append(item)
-
-        # Add data to cache
-        CACHE.set(key, data)
-    else:
-        data = cache_value
+    data = _lastcontact('DoRoad')
 
     # Return
     return jsonify(data)
@@ -73,31 +47,59 @@ def get_lastgps_riders():
 
     """
     # Initialize key variables
+    data = _lastcontact('OneStop')
+
+    # Return
+    return jsonify(data)
+
+
+def _lastcontact(agent_name):
+    """Get last GPS coordinates for drivers.
+
+    Args:
+        None
+
+    Returns:
+        results: A list of dicts of results
+
+    """
+    # Initialize key variables
+    lookuptable = defaultdict(lambda: defaultdict(dict))
+    lastcontacts = None
     data = []
 
     # Get data from cache
-    key = ('mdl.api:get/coordinates/lastcontactsriders')
+    key = ('mdl.api:get/coordinates/lastcontactsdrivers')
     cache_value = CACHE.get(key)
-
-    # Get infoset URI to process
-    uri = 'db/multitable/datapointsummary'
-    url = '{}/{}'.format(reference.infoset_api_url_prefix(CONFIG), uri)
 
     # Process cache miss
     if cache_value is None:
-        # Get data from infoset
+        # Get lookup table of datapoints
+        lookuptable = _lastcontact_lookup(agent_name)
+
+        # Get infoset last contact information
+        uri = 'lastcontacts'
+        url = '{}/{}'.format(reference.infoset_api_url_prefix(CONFIG), uri)
         try:
             result = requests.get(url)
-            received_data = result.json()
+            lastcontacts = result.json()
         except:
-            received_data = None
+            lastcontacts = None
 
-        # Filter data
-        if received_data is not None:
-            for item in received_data:
-                if item['name'] != 'OneStop':
-                    continue
-                data.append(item)
+        if bool(lastcontacts) is True:
+            # Create report
+            for item in lastcontacts:
+                # Assign values
+                idx_datapoint = item['idx_datapoint']
+                data_dict = {}
+                data_dict['idx_datapoint'] = idx_datapoint
+                data_dict['value'] = item['value']
+                data_dict['timestamp'] = item['timestamp']
+                for key, value in lookuptable[idx_datapoint].items():
+                    data_dict[key] = value
+
+                # Append to report
+                data.append(data_dict)
 
         # Add data to cache
         CACHE.set(key, data)
@@ -105,4 +107,45 @@ def get_lastgps_riders():
         data = cache_value
 
     # Return
-    return jsonify(data)
+    return data
+
+
+def _lastcontact_lookup(agent_name):
+    """Get last contact data from all agents of specific name.
+
+    Args:
+        agent_name: Name of agent
+
+    Returns:
+        lookuptable: Dict of agent and datapoint information keyed by
+            idx_datapoint
+
+    """
+    # Initialize key variables
+    lookuptable = defaultdict(lambda: defaultdict(dict))
+    datapointsummary = None
+
+    # Get infoset datapoint information
+    uri = 'datapoints/all/summary'
+    url = '{}/{}'.format(reference.infoset_api_url_prefix(CONFIG), uri)
+    try:
+        result = requests.get(url)
+        datapointsummary = result.json()
+    except:
+        datapointsummary = None
+
+    # Create a lookup table for datapoints
+    if bool(datapointsummary) is True:
+        for item in datapointsummary:
+            # Only process the desired agent
+            if item['name'] != agent_name:
+                continue
+
+            # Append data
+            idx_datapoint = item['idx_datapoint']
+            for key, value in item.items():
+                if key != 'idx_datapoint':
+                    lookuptable[idx_datapoint][key] = value
+
+    # Abort if nothing found
+    return lookuptable
