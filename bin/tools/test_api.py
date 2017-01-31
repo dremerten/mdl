9e9dcf2f59e317e047e5b78c8a52d0a197fdd27d
Peter Harrison <blackriver@simiya.com>
2017-01-30 16:36:55 -0800

    Fixes #5

--- bin/tools/test_api.py
+++ bin/tools/test_api.py
@@ -3,6 +3,19 @@
 
 # Standard imports
 import sys
+import os
+
+# Try to create a working PYTHONPATH
+script_directory = os.path.dirname(os.path.realpath(__file__))
+bin_directory = os.path.abspath(os.path.join(script_directory, os.pardir))
+root_directory = os.path.abspath(os.path.join(bin_directory, os.pardir))
+if script_directory.endswith('/mdl/bin/tools') is True:
+    sys.path.append(root_directory)
+else:
+    print(
+        'This script is not installed in the "mdl/bin/tools" '
+        'directory. Please fix.')
+    sys.exit(2)
 
 # mdl imports
 try:
